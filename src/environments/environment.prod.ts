cb3412b82c085fb370b582304fe91072506d1ff4
Benjie Penol <benjie.penol@gmail.com>
2026-04-28 12:28:08 +0800

    new suppabase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   supabase: {
-   url: 'https://zhuthwapkyskuikkbztf.supabase.co',
+   url: 'https://xgxdlacrgzgyvnwdwncr.supabase.co',
    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpodXRod2Fwa3lza3Vpa2tienRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NzgyNzMsImV4cCI6MjA5MjM1NDI3M30.phZL4Lg1MoWqShjC7_Iev-PHZGrWetsO0I-MYXFQ2K8'
 
   }
 };
+
+https://xgxdlacrgzgyvnwdwncr.supabase.co
